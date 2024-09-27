653cceafdd94264204deab459dd40fbeb6ba7431
Imran-2022 <mdimranulhaque202@gmail.com>
2024-09-28 03:33:19 +0600

    completing auth

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -118,9 +118,9 @@ const Contact = () => {
             />
           </div>
           <div>
-            <button
+            <button disabled
               type="submit"
-              className="w-full bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              className=" w-full bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
             >
               Submit
             </button>
